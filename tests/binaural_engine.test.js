cdd85ca24d0be5aa1912a331e19e48f3e7b7dd9c
Meistro <meistro57@gmail.com>
2025-07-01 15:23:26 -0500

    Add drift mode and focus slider

---
+++ tests/binaural_engine.test.js
@@ -0,0 +1,41 @@
+const BinauralEngine = require('../binaural_engine');
+
+jest.useFakeTimers();
+
+describe('BinauralEngine', () => {
+  test('start initializes oscillators', () => {
+    const engine = new BinauralEngine();
+    engine.start(100, 4, 0.8);
+    expect(engine.leftOsc.frequency.value).toBe(100);
+    expect(engine.rightOsc.frequency.value).toBe(104);
+    expect(engine.gainNode.gain.value).toBeCloseTo(0.8);
+    engine.stop();
+  });
+
+  test('update modifies frequencies', () => {
+    const engine = new BinauralEngine();
+    engine.start(100, 4);
+    engine.update(110, 5);
+    expect(engine.leftOsc.frequency.value).toBe(110);
+    expect(engine.rightOsc.frequency.value).toBe(115);
+    engine.stop();
+  });
+
+  test('setVolume changes gain value', () => {
+    const engine = new BinauralEngine();
+    engine.setVolume(0.2);
+    expect(engine.gainNode.gain.value).toBeCloseTo(0.2);
+  });
+
+  test('drift mode sweeps beat frequency', () => {
+    const engine = new BinauralEngine();
+    engine.start(100, 3);
+    engine.startDrift(1, 3, 7);
+    expect(engine.rightOsc.frequency.value).toBeCloseTo(103);
+    jest.advanceTimersByTime(500);
+    expect(engine.rightOsc.frequency.value).toBeCloseTo(107, 1);
+    jest.advanceTimersByTime(500);
+    expect(engine.rightOsc.frequency.value).toBeCloseTo(103, 1);
+    engine.stop();
+  });
+});
